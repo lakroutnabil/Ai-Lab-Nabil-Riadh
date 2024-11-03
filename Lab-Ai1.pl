9ab8d0ed3986b0035bcc737958c09fc633673db1
Lakrout Nabil Abdeldjabar <lakrout.nabil7@gmail.com>
2024-11-03 23:19:46 +0100

    I'll come back for it in few

--- Lab-Ai1.pl
+++ Lab-Ai1.pl
@@ -1,1 +1,46 @@
-%Just having fun once moooooore! 
+% Task 01
+:- dynamic task/4.
+% Task 02
+create_task(ID, Description, Assignee) :-
+    assertz(task(ID, Description, Assignee, false)),
+    format('Task ~w created: ~w.~n', [ID, Description]).
+% Task 03
+assign_task(ID, NewAssignee) :-
+    retract(task(ID, Description, _, Completed)),
+    assertz(task(ID, Description, NewAssignee, Completed)),
+    format('Task ~w assigned to user: ~w.~n', [ID, NewAssignee]).
+% Task 04
+mark_completed(ID) :-
+    retract(task(ID, Description, Assignee, _)),
+    assertz(task(ID, Description, Assignee, true)),
+    format('Task ~w marked as completed.~n', [ID]).
+
+% Task 05
+DisplayTasks :-
+    forall(task(ID, Description, Assignee, Completed), (
+        format('Task ~w:~n - Description: ~w~n - Assignee: ~w~n - Completion status: ~w~n~n',
+        [ID, Description, Assignee, Completed])
+    )).
+% Task 06
+display_tasks_assigned_to(User) :-
+    format('Tasks assigned to ~w:~n', [User]),
+    forall(task(ID, Description, User, Completed), (
+        format('Task ~w:~n - Description: ~w~n - Completion status: ~w~n~n',
+        [ID, Description, Completed])
+    )).
+
+% Task 07 
+% dok n3awed na7iha
+display_completed_tasks :-
+    writeln('Completed tasks:'),
+    forall(task(ID, Description, Assignee, true), (
+        format('Task ~w:~n - Description: ~w~n - Assignee: ~w~n~n',
+        [ID, Description, Assignee])
+    )).
+?- create_task(1, 'Implement login functionality', 'Alice').
+?- create_task(2, 'Design homepage layout', 'Bob').
+?- assign_task(1, 'John').
+?- mark_completed(2).
+?- display_tasks.
+?- display_tasks_assigned_to('Alice').
+?- display_completed_tasks.
