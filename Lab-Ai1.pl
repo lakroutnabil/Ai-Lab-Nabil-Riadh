577e1d2b9eee74a4ee16070f37e2789325e74f60
riadhrahmoune007 <161158131+riadhrahmoune007@users.noreply.github.com>
2024-11-03 23:11:47 +0100

    Update Lab-Ai1.pl

--- Lab-Ai1.pl
+++ Lab-Ai1.pl
@@ -1,2 +1,16 @@
-%Just having fun once moooooore! 
-%gooool wallah
+% Task 01
+:- dynamic task/4.
+% Task 02
+create_task(ID, Description, Assignee) :-
+    assertz(task(ID, Description, Assignee, false)),
+    format('Task ~w created: ~w.~n', [ID, Description]).
+% Task 03
+assign_task(ID, NewAssignee) :-
+    retract(task(ID, Description, _, Completed)),
+    assertz(task(ID, Description, NewAssignee, Completed)),
+    format('Task ~w assigned to user: ~w.~n', [ID, NewAssignee]).
+% Task 04
+mark_completed(ID) :-
+    retract(task(ID, Description, Assignee, _)),
+    assertz(task(ID, Description, Assignee, true)),
+    format('Task ~w marked as completed.~n', [ID]).
