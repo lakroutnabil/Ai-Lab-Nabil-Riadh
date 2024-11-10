cf165a7af0e55cd551c228e7f66c13fc4718275b
Nabil <108232457+lakroutnabil@users.noreply.github.com>
2024-11-10 21:54:01 +0100

    Update Lab-Ai2.pl

--- Lab-Ai2.pl
+++ Lab-Ai2.pl
@@ -12,3 +12,13 @@ penultimate(X, [_|T]) :- penultimate(X, T).
 %Task05
 del_k(X, [X|T], 1, T).
 del_k(X, [H|T], K, [H|R]) :- K > 1, K1 is K - 1, del_k(X, T, K1, R).
+%Task06
+length([], 0).
+length([_|T], N) :- length(T, N1), N is N1 + 1.
+%Task07
+even(L) :- length(L, N), 0 is N mod 2.
+%Task08
+concat([], L, L).
+concat([H|T], L, [H|R]) :- concat(T, L, R).
+%Task09
+palindrome(L) :- reverse(L, L).
